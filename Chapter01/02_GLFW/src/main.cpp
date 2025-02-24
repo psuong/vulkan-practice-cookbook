89a9dfb3c2682e19ccdbfa4c6f9b86e85b78ab94
Porrith Suong <porrith.suong@gmail.com>
2025-02-24 00:39:46 -0500

    Finishes GLFW chapter.

--- Chapter01/02_GLFW/src/main.cpp
+++ Chapter01/02_GLFW/src/main.cpp
@@ -1,6 +1,16 @@
+#include <shared/HelpersGLFW.h>
 #include <stdio.h>
 
 int main() {
-    printf("TODO!\n");
-    return 0;
+	uint32_t w = 1280;
+	uint32_t h = 720;
+	GLFWwindow* window = initWindow("GLFW Window", w, h);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
